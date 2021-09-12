7364f00a299a52b0677f7e6477758b79b0d1363c
omokun <omokun1215@gmail.com>
2021-09-12 18:07:17 +0900

    AJAX作成

--- app/controllers/informations_controller.rb
+++ app/controllers/informations_controller.rb
@@ -6,6 +6,7 @@ class InformationsController < ApplicationController
 
   def create
     @information = SubIssue.find(params[:sub_issue_id]).informations.create(information_params)
+    # redirect_to controller: 'sub_issues', action: 'index'
   end
 
   private
--- app/controllers/sub_issues_controller.rb
+++ app/controllers/sub_issues_controller.rb
@@ -5,21 +5,19 @@ class SubIssuesController < ApplicationController
     @sub_issue = SubIssue.new
     @main_issue = MainIssue.find(params[:main_issue_id])
     @sub_issues = @main_issue.sub_issues.includes(:main_issue)
-
-    @information = Information.new
-    @informations = @sub_issue.informations.includes(:sub_issue)
   end
 
   def create
     @main_issue = MainIssue.find(params[:main_issue_id]).sub_issues.create(sub_issue_params)
     # @sub_issue = @main_issue.sub_issues.new(sub_issue_params)
     # if @sub_issue.save
+    redirect_to action: :index
   end
 
   def update
     sub_issue = SubIssue.eager_load(:main_issue).find(params[:id])
     sub_issue.update!(sub_issue_params)
-    # redirect_to action: :index
+    redirect_to action: :index
   end
 
   private
